16f9ba7b9859048b5af9c5f38bee5c7174e3a983
Muhammad Arizky <arizkymuhammad46@gmail.com>
2024-06-25 09:51:43 +0700

    update products layout

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -3,7 +3,7 @@ import Title from 'antd/es/typography/Title'
 import React from 'react'
 
 async function getProducts() {
-    const result = await fetch('https://fakestoreapi.com/products')
+    const result = await fetch('https://fakestoreapi.com/products?limit=8')
 
     if (!result.ok) throw new Error("Failed to fetch data.")
 
@@ -13,11 +13,10 @@ async function getProducts() {
 export default async function ProductPage() {
     const products = await getProducts()
     return (
-        <div>
+        <div className=''>
             <Title level={3}>Our Product</Title>
 
-
-            <div className='bg-black block'>
+            <div className='grid gap-3 grid-cols-4 mt-7 ml-8'>
                 {products.length > 0 && products.map((product: any) => (
                     <div key={product.id}>
                         <CardProduct title={product.title} price={product.price} description={product.description} image={product.image} />
--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -1,41 +1,23 @@
 'use client'
 
-import React, { useState } from 'react';
-import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    InfoCircleOutlined,
-    HomeOutlined,
-    ContactsOutlined,
-} from '@ant-design/icons';
+import React from 'react';
 import { Button, Layout, Menu, theme } from 'antd';
-import Link from 'next/link';
 import Sidebar from './Siderbar';
+import { Footer } from 'antd/es/layout/layout';
 
 const { Header, Sider, Content } = Layout;
 
 export default function Template({ children }: { children: React.ReactNode }) {
-    const [collapsed, setCollapsed] = useState(false);
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
     return (
-        <Layout className='h-screen'>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
+        <Layout>
+            <Sider trigger={null} style={{ overflow: 'auto', height: '100vh', position: 'fixed' }}>
                 <Sidebar />
             </Sider>
-            <Layout>
+            <Layout style={{ marginLeft: 200 }}>
                 <Header style={{ padding: 0, background: colorBgContainer }}>
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
                 </Header>
                 <Content
                     style={{
@@ -48,6 +30,9 @@ export default function Template({ children }: { children: React.ReactNode }) {
                 >
                     {children}
                 </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Store App ©{new Date().getFullYear()} Created by Efek Tenang
+                </Footer>
             </Layout>
         </Layout>
     )
